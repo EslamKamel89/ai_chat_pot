e3f28f7e3d3f76f19ee76a3bbce38bfcd8496395
EslamKamel89 <elsamkamelforex@gmail.com>
2025-05-24 12:21:38 +0300

    refactor: Remove unused imports and commented-out HydratedBloc storage initialization in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,25 +5,22 @@ import 'package:ai_chat_pot/core/router/app_routes_names.dart';
 import 'package:ai_chat_pot/core/service_locator/service_locator.dart';
 import 'package:ai_chat_pot/core/themes/theme_cubit.dart';
 import 'package:easy_localization/easy_localization.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:intl/intl_standalone.dart';
-import 'package:path_provider/path_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initServiceLocator();
   await findSystemLocale();
   await EasyLocalization.ensureInitialized();
-  HydratedBloc.storage = await HydratedStorage.build(
-    storageDirectory:
-        kIsWeb
-            ? HydratedStorageDirectory.web
-            : HydratedStorageDirectory((await getTemporaryDirectory()).path),
-  );
+  // HydratedBloc.storage = await HydratedStorage.build(
+  //   storageDirectory:
+  //       kIsWeb
+  //           ? HydratedStorageDirectory.web
+  //           : HydratedStorageDirectory((await getTemporaryDirectory()).path),
+  // );
   runApp(
     EasyLocalization(
       supportedLocales: const [
